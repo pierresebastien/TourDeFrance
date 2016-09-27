322cfa7998ba8c7b8811dbdda894bfe96a586574
Sébastien Pierre <pierre.sebastien@gmail.com>
2016-09-27 12:52:53 +0200

    Improve angular side bar

--- TourDeFrance.Ui.Angular/app/shared/menu.ts
+++ TourDeFrance.Ui.Angular/app/shared/menu.ts
@@ -1,12 +1,42 @@
 ﻿/// <reference path="../../references.ts"/>
 
 class MenuController extends BaseController {
+	sections: any[];
+
 	constructor(Restangular: restangular.IService,
 		GlobalService: tourdefrance.services.IGlobalService,
 		$state: ng.ui.IStateService,
 		$mdToast: ng.material.IToastService,
+		gettextCatalog: angular.gettext.gettextCatalog,
 		currentUser: AuthenticatedUser) {
 
-		super(Restangular, GlobalService, $state, $mdToast, currentUser);
+		super(Restangular, GlobalService, $state, $mdToast, gettextCatalog, currentUser);
+
+		this.sections = [
+			{
+				name: "Home",
+				subSections: [
+					{ name: this.gettextCatalog.getString("Games"), icon: "bicycle", route: "root.games" },
+					{ name: this.gettextCatalog.getString("History"), icon: "trophy", route: "root.history" }
+				]
+			},
+			{
+				name: "Management",
+				subSections: [
+					{ name: this.gettextCatalog.getString("Drinks"), icon: "beer", route: "root.management.drinks" },
+					{ name: this.gettextCatalog.getString("Stages"), icon: "map-marker", route: "root.management.stages" },
+					{ name: this.gettextCatalog.getString("Races"), icon: "map-o", route: "root.management.races" },
+					{ name: this.gettextCatalog.getString("Riders"), icon: "user", route: "root.management.riders" },
+					{ name: this.gettextCatalog.getString("Teams"), icon: "users", route: "root.management.teams" }
+				]
+			},
+			{
+				name: "Administration",
+				subSections: [
+					{ name: this.gettextCatalog.getString("Users"), icon: "user", route: "root.admin.users" },
+					{ name: this.gettextCatalog.getString("Config"), icon: "wrench", route: "root.admin.config" }
+				]
+			}
+		];
 	}
 }
